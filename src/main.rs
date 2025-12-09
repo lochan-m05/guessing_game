ec37be5820b8ea627af861e985f993bd860b7daf
lochan m <m.lochan11@outlook.in>
2025-12-09 22:39:52 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,39 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+const MIN_RANGE: u32 = 1;
+const MAX_RANGE: u32 = 1000;
+
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number between {} and {}!", MIN_RANGE, MAX_RANGE);
 
-    // Generate a secret number
-    let secret_number = rand::thread_rng().gen_range(1..=1000);
+    let secret_number = rand::thread_rng().gen_range(MIN_RANGE..=MAX_RANGE);
 
-    
     loop {
         println!("Please input your guess:");
 
-        // Read user input
         let mut guess = String::new();
+        
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        
-       let guess: u32 = match guess.trim().parse() {
+
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please type a number!");
+                println!("Invalid input. Please type a number.");
                 continue;
             }
         };
 
+     
+        if guess < MIN_RANGE || guess > MAX_RANGE {
+            println!("Out of bounds! Stick to {}-{}", MIN_RANGE, MAX_RANGE);
+            continue;
+        }
+
         println!("You guessed: {}", guess);
 
-        
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -38,4 +44,4 @@ fn main() {
             }
         }
     }
-}   
+}
